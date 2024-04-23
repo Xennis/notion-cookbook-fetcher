f848d6656fcdc6ce6e93586bd1ac4056133f36b7
Xennis <code@xennis.org>
2024-04-23 16:05:24 +0200

    Add HelloWorld cloud function

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # notion-cookbook-fetcher
+
+## Development
+
+### Deployment
+
+* [Install the gcloud CLI](https://cloud.google.com/sdk/docs/install) and login: `gcloud auth login`
+* Set the default project: `gcloud config set project <PROJECT_ID>`
+* Start the deployment: `npm run deploy`
---
+++ index.js
@@ -0,0 +1,8 @@
+const functions = require('@google-cloud/functions-framework');
+
+// Register an HTTP function with the Functions Framework that will be executed
+// when you make an HTTP request to the deployed function's endpoint.
+functions.http('updateGET', (req, res) => {
+  res.send('Hello World!');
+});
+
